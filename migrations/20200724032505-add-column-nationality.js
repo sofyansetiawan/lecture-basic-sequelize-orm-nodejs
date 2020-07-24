eba054b2b01001795be77222d85009cb28e6554c
Sofyan <sofyan@MacBook-Air-Sofyan.local>
2020-07-24 10:43:46 +0700

    for student's references

---
+++ migrations/20200724032505-add-column-nationality.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+     return queryInterface.addColumn('People', 'Nationality', Sequelize.STRING, {
+      after: 'address'
+   });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    return queryInterface.removeColumn('People', 'Nationality');
+  }
+};
